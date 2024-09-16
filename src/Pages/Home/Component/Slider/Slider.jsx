1c4b27f82b9c302a0ee06315d8388da3264c2d40
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-09-16 11:26:40 -0700

    pro

--- src/Pages/Home/Component/Slider/Slider.jsx
+++ src/Pages/Home/Component/Slider/Slider.jsx
@@ -30,7 +30,7 @@ function Slider() {
 
 
 
-      <section className=" mb-10 bg-[#8DAAA6] relative">
+      <section className=" mb-10 bg-[#025048] relative">
 
         <div className=" w-[90%] mx-auto flex p-8 justify-between">
 
@@ -39,7 +39,7 @@ function Slider() {
 
           <div style={{ fontFamily: 'cursive' }} className=" text-4xl w-[650px] text-white mt-16 ">
 
-            <p className=" text-7xl">One of a <span className=" text-[#ffb6ad]">Kind</span></p><p className=" ms-16 leading-normal text-7xl"> Just Like <span className=" text-[#ffb6ad]">You</span>.</p>
+            <p className=" text-7xl">One of a <span className=" text-[#f18f55]">Kind</span></p><p className=" ms-16 leading-normal text-7xl"> Just Like <span className=" text-[#ffb6ad]">You</span>.</p>
             <p className=" text-2xl text-[#dceef1] mt-10  leading-normal">"Mashrabiya" offers unique handmade crafts and antiques, blending heritage and creativity. we connect you to the world through authentic artistry.</p>
             {/* Discover timeless treasures and handcrafted wonders that tell a story of artistry and tradition, each piece uniquely made to add charm and character to your home." */}
             {/* **"مشربية"** تقدم منتجات يدوية فريدة وأنتيكات، تجمع بين التراث والإبداع. مستوحاة من النافذة التقليدية، نربطك بالعالم من خلال فنون أصيلة. */}
@@ -56,8 +56,8 @@ function Slider() {
 
 
         <div className="custom-shape-divider-bottom-1726398691">
-          <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
           </svg>
         </div>
       </section>
--- src/components/Art/ArtProfile.jsx
+++ src/components/Art/ArtProfile.jsx
@@ -15,12 +15,45 @@ function ArtProfile() {
   }, [user]);
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100  ">
       {user ? (
-        <div>
-          <h1>{user.firstname} {user.lastname}</h1>
-          <p>{user.about}</p>
-          <img src={user.profilePic || "default-avatar.png"} alt="Profile" />
+        <div className=" mx-auto bg-white shadow-lg rounded-lg overflow-hidden pt-44">
+          <div className="relative">
+          
+           
+          </div>
+
+          <div className="p-20">
+            <div className="relative">
+              <div className="absolute -top-40 left-20">
+                <img
+            src={user.profilePic || "https://th.bing.com/th/id/OIP.PW1QzPVwoZHjpHacJ3WjjwAAAA?rs=1&pid=ImgDetMain"}
+            alt="Profile"
+                  className="w-80 h-80 rounded-full object-cover border-4 border-white shadow-lg"
+                />
+              </div>
+
+              <div className="justfy-center items-center ml-96 pl-16">
+                <h1 className="text-2xl font-bold text-gray-800 pb-4">{user.firstname} {user.lastname}</h1>
+                <p className="text-gray-600 pb-4">{user.email}</p>
+                <p className="text-gray-600 pb-4">{user.accountType}</p>
+
+                <p className="text-gray-600 pb-4">{user.about}</p>
+
+
+
+               
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-4 px-4">
+            <ul className="flex space-x-4 text-gray-600">
+              <li className="hover:text-blue-500 cursor-pointer">Events</li>
+              <li className="hover:text-blue-500 cursor-pointer">Posts</li>
+              
+            </ul>
+          </div>
         </div>
       ) : (
         <p>No user data available</p>
