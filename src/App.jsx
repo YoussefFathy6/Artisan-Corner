9d4dea6af2c3790bddb42f67c5eeb2e7f545607d
hayataboelhassan <hayataboelhassan78@gmail.com>
2024-10-06 16:14:50 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, createContext, useContext } from "react";
+import React, { useEffect, useState, createContext} from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./Pages/Home/Home";
 import Footer from "./components/Footer/Footer";
@@ -45,13 +45,13 @@ import "react-toastify/dist/ReactToastify.css";
 import Side from "./Pages/Profile/Side";
 import CssBaseline from '@mui/material/CssBaseline';
 
-// استدعاء الثيمات من ملف themes.js
+// Importing themes from theme.js
 import { darkTheme, lightTheme } from './theme';
 
 export const ThemeContext = createContext();
 
 import Setting from "./Pages/Profile/Setting";
-// import ChatApp from "./components/Chat/ChatApp";
+
 function App() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -129,6 +129,7 @@ function App() {
                     <Route path="/adddeitalsprofile" element={<AddDeitalsprofile />} />
                     <Route path="/accountbalance" element={<Accountbalance />} />
                     <Route path="/Side" element={<Side />} />
+                    <Route path="/setting" element={<Setting />} />
                   </Routes>
                   <Footer />
                   <ToastContainer />
@@ -139,62 +140,6 @@ function App() {
         </Paper>
       </ThemeProvider>
     </ThemeContext.Provider>
-    <>
-      <ReviewsProvider>
-        <RatingsProvider>
-          {isAdmin ? (
-            <DashBoard />
-          ) : (
-            <>
-              <NavBar />
-              <Routes>
-                <Route path="chat" element={<Chat />} />
-                {/* <Route path="chatApp" element={<ChatApp />} /> */}
-
-                <Route path="/" element={<Home />} />
-                <Route path="posts" element={<Posts />} />
-
-                <Route path="earnings" element={<EarningsPage />} />
-                <Route path="shipping" element={<ShippingPage />} />
-                <Route path="payment" element={<CheckoutPage />} />
-                <Route path="register" element={<RegisterPage />} />
-                <Route path="special" element={<SpecialOrderPage />} />
-                <Route path="auction" element={<AuctionPage />} />
-                <Route path="proposals" element={<ProposalsPage />} />
-                <Route path="verify" element={<VerificationPage />} />
-                <Route path="/details" element={<Details />} />
-                <Route path="/bag" element={<ProductBag />} />
-                <Route path="order" element={<Order />} />
-                <Route path="*" element={<NotFound />} />
-                <Route path="/ticket" element={<Ticket />} />
-                <Route path="/event" element={<AllEvent />} />
-                <Route path="/setting" element={<Setting/>} />
-                <Route
-                  path="/TicketConfirmation/:eventId"
-                  element={<TicketConfirmation />}
-                />
-                <Route path="/Artprofile" element={<ArtProfile />} />
-                <Route path="/online" element={<VideoCall />} />
-                <Route path="/EventOnline" element={<EventOnline />} />
-                <Route path="/Users" element={<Users />} />
-                <Route
-                  path="/TicketOnline/:eventId"
-                  element={<TicketOnline />}
-                />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/eventuser" element={<Eventuser />} />
-                <Route
-                  path="/adddeitalsprofile"
-                  element={<AddDeitalsprofile />}
-                />
-              </Routes>
-              <Footer />
-              <ToastContainer />
-            </>
-          )}
-        </RatingsProvider>
-      </ReviewsProvider>
-    </>
   );
 }
 
